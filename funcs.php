32a5b8705493dd7f0481a4c775ee86d88ad04469
toshiharocky <toshiharu69@gmail.com>
2021-04-06 11:07:16 +0900

    関数追加

--- funcs.php
+++ funcs.php
@@ -7,3 +7,25 @@ function h($str)
     return htmlspecialchars($str, ENT_QUOTES);
 }
 
+
+function dbconn(){
+    try {
+        //Password:MAMP='root',XAMPP=''
+        $pdo = new PDO('mysql:dbname=inventory_management;charset=utf8;host=localhost','root','root');
+        return $pdo;
+    } catch (PDOException $e) {
+        exit('DBConnectError'.$e->getMessage());
+    }
+}
+
+//SQLエラー関数：sql_error($stmt)
+function sql_error($stmt){
+    $error = $stmt->errorInfo();
+    exit("SQLError:" . print_r($error, true));
+}
+
+//リダイレクト関数: redirect($file_name)
+function redirect($file_name){
+    header("Location: $file_name");
+    exit();
+}
